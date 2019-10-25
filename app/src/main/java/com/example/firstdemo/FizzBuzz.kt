47447459a200315650f0cac9697bf2bbe686a390
chenweiqiang <464306572@qq.com>
2019-10-25 22:59:52 +0800

    fizz buzz kata

---
+++ app/src/main/java/com/example/firstdemo/FizzBuzz.kt
@@ -0,0 +1,19 @@
+package com.example.firstdemo
+
+class FizzBuzz(private val i: Int) {
+    fun of(): String {
+        if (isDivisibleBy(3) && isDivisibleBy(5)) {
+            return "fizzbuzz"
+        }
+        if (isDivisibleBy(3)) {
+            return "fizz"
+        }
+        if (isDivisibleBy(5)) {
+            return "buzz"
+        }
+
+        return i.toString()
+    }
+
+    private fun isDivisibleBy(num: Int) = i % num == 0
+}
---
+++ app/src/test/java/com/example/firstdemo/FizzBuzzTest.kt
@@ -0,0 +1,33 @@
+package com.example.firstdemo
+
+import org.hamcrest.core.Is
+import org.junit.Assert.assertThat
+import org.junit.Test
+
+
+class FizzBuzzTest {
+    @Test
+    fun should_print_number_when_given_real_number() {
+        checkNumber(1, "1")
+        checkNumber(2, "2")
+    }
+
+    @Test
+    fun should_print_fizz_when_given_number_divisible_three() {
+        checkNumber(3, "fizz")
+    }
+
+    @Test
+    fun should_print_buzz_when_given_number_divisible_five() {
+        checkNumber(5, "buzz")
+    }
+
+    @Test
+    fun should_print_fizz_buzz_when_given_number_divisible_three_and_five() {
+        checkNumber(15, "fizzbuzz")
+    }
+
+    private fun checkNumber(i: Int, excepted: String) {
+        assertThat(FizzBuzz(i).of(), Is.`is`(excepted))
+    }
+}
